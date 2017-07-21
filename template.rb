b566667052a498d9c7abf995975984bfa3480978
kota718 <kota.yt@gmail.com>
2017-07-21 22:17:45 +0900

    Add Ruby version.

--- template.rb
+++ template.rb
@@ -2,3 +2,10 @@
 
 # .gitignore
 run 'gibo macOS Ruby Rails JetBrains > .gitignore'
+
+# Ruby version
+ruby_version = `ruby -v`.scan(/\d\.\d\.\d/).flatten.first
+insert_into_file 'Gemfile',%(
+
+ruby '#{ruby_version}'), after: "source 'https://rubygems.org'"
+run "echo '#{ruby_version}' > ./.ruby-version"
