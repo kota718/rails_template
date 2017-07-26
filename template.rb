b03ead2485f62eaf04d179e8017e4e245318f061
Kota Seki <kota.yt@gmail.com>
2017-07-26 19:24:43 +0900

    Update template.rb

--- template.rb
+++ template.rb
@@ -109,6 +109,21 @@ insert_into_file 'config/environments/development.rb',%(
   end
 ), after: 'config.assets.debug = true'
 
+insert_into_file 'config/boot.rb',%(
+  if ENV['RAILS_ENV'] == 'development'
+    require 'bootsnap'
+    Bootsnap.setup(
+      cache_dir: 'tmp/cache',
+      development_mode: true,
+      load_path_cache: true,
+      autoload_paths_cache: true,
+      disable_trace: false,
+      compile_cache_iseq: true,
+      compile_cache_yaml: true,
+    )
+ +end
+), after: "require 'bundler/setup' # Set up gems listed in the Gemfile."
+
 run 'bundle exec spring binstub --all'
 
 run 'bundle exec rubocop --auto-correct'
